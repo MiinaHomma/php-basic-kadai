019059ac6693898da531d041d3125698b7785832
MiinaHomma <miinahomma@gmail.com>
2025-07-22 19:48:35 +0900

    Add files via upload

---
+++ associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <p>
+   <?php
+   $vegetable =['name' => 'onion', 'price' => 200, 'weight' => '160'];
+
+   echo '<pre>';
+   print_r($vegetable);
+   echo '</pre>';
+   ?>
+  </p>
+</body>
+
+</html>
